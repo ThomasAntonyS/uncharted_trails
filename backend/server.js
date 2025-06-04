f003ac40f80fe91f2b3f5dbfb06bc06b7cbe229a
Thomas Antony S <srtomsam@gmail.com>
2025-06-04 20:40:53 +0530

    updated server

--- backend/server.js
+++ backend/server.js
@@ -17,12 +17,12 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin) return callback(null, true); 
-    if (allowedOrigins.indexOf(origin) === -1) {
-      return callback(new Error("CORS policy does not allow this origin"), false);
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS policy does not allow this origin"), false); // Block the request
     }
-    return callback(null, true);
   },
   credentials: true
 }));
