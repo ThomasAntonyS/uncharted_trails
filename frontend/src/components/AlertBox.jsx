ebcecd7e3ce393a2d8d18dd361ce9c8acc56330a
Thomas Antony S <srtomsam@gmail.com>
2025-06-10 10:31:59 +0530

    updated alert box colors

--- frontend/src/components/AlertBox.jsx
+++ frontend/src/components/AlertBox.jsx
@@ -1,8 +1,8 @@
 import React, { useContext, useEffect } from 'react';
 import { UserContext } from '../Context/UserContextProvider';
 import { BiSolidErrorAlt } from "react-icons/bi";
-import { TiTick } from "react-icons/ti";
 import { IoMdClose } from "react-icons/io";
+import { BsCheck2Circle } from "react-icons/bs";
 
 
 const AlertBox = () => {
@@ -31,30 +31,30 @@ const AlertBox = () => {
     }
 
     return (
-        <div className="fixed top-4 right-4 z-[100]
-                        bg-white shadow-lg p-4 rounded-md border border-gray-200
+        <div className={`fixed top-4 right-4 z-[100] ${alertBox.isError? 'bg-[#FF3F33]':'bg-[#44c662]' }
+                        shadow-lg p-4 rounded-md border
                         flex items-center gap-3
                         max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl w-auto
                         font-poppins
-                        animate-slideInFromRight"
+                        animate-slideInFromRight`}
         >
             {alertBox.isError ?
-                <BiSolidErrorAlt className="text-red-700 w-6 h-6 flex-shrink-0" />
+                <BiSolidErrorAlt className="text-white w-6 h-6 flex-shrink-0" />
                 :
-                <TiTick className="text-blue-800 w-6 h-6 flex-shrink-0" />
+                <BsCheck2Circle className="text-white w-6 h-6 flex-shrink-0" />
             }
 
             <div className="flex flex-col flex-grow">
                 <div className="flex items-center justify-between w-full">
-                    <span className="text-gray-800 text-sm sm:text-base pr-2">
+                    <span className="text-white text-sm sm:text-base pr-2">
                         {alertBox.message}
                     </span>
                     <button
                         onClick={handleCloseAlert}
-                        className="text-gray-500 hover:text-gray-700 flex-shrink-0 p-1 rounded-full hover:bg-gray-100 transition-colors duration-200"
+                        className="text-white hover:text-gray-700 flex-shrink-0 p-1 rounded-full hover:bg-black/50 transition-colors duration-200"
                         aria-label="Close alert"
                     >
-                        <IoMdClose className="w-5 h-5 text-black" />
+                        <IoMdClose className="w-5 h-5 text-white" />
                     </button>
                 </div>
             </div>
