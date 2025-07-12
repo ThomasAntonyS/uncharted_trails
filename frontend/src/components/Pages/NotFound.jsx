e2a269aed28347b82e3cfc09edd895e48f06feea
Thomas Antony S <srtomsam@gmail.com>
2025-07-12 16:36:38 +0530

    updated 404-page component

--- frontend/src/components/Pages/NotFound.jsx
+++ frontend/src/components/Pages/NotFound.jsx
@@ -11,15 +11,17 @@ const NotFound = () => {
     >
 
       {/* Error Content Box */}
-      <div className="relative h-screen w-screen z-10 flex flex-col items-center justify-center backdrop-blur-md shadow-lg rounded-lg text-center tracking-wide text-white">
+      <div className="relative h-screen w-screen z-10 flex flex-col items-center justify-center backdrop-blur-md shadow-lg rounded-lg text-center tracking-wide text-white font-medium">
         
-        <h2 className=" text-2xl sm:text-[2rem] font-semibold tracking-wide uppercase font-libreCaslon">FOG ERROR</h2>
+        <h2 className=" text-2xl sm:text-[2rem] font-semibold tracking-wide uppercase font-libreCaslon">Lost in the Mist</h2>
         <h1 className=" text-7xl sm:text-[6rem] font-bold mt-2 font-agdasima">404</h1>
         <p className=" text-2xl sm:text-[3rem] mt-2 font-poppins">Page not found</p>
-        <p className=" text-1xl w-[90%] sm:text-[1.5rem] mt-1 font-poppins">I tried to catch some fog, but I missed it</p>
+        <p className=" text-1xl w-[90%] sm:text-[1.5rem] mt-4 font-poppins">We tried to guide you to your dream destination, but the path got a little foggy.</p>
+        <p className=" text-1xl w-[90%] sm:text-[1.5rem] font-poppins">Let’s get you back on track </p>
+
 
         {/* Back to Home Button */}
-        <Link to="/" className="mt-6 sm:text-[1.2rem] px-6 py-2 font-agdasima border border-white text-white rounded-lg hover:bg-white hover:text-black transition">
+        <Link to="/" className="mt-6 sm:text-[1.2rem] px-6 py-2 font-agdasima font-semibold border border-white text-white bg-white/20 rounded-lg hover:bg-white hover:text-black transition">
           Back to Home
         </Link>
 
